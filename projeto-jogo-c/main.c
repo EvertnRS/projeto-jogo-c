6e4e0fa7635dfd6fd8005c00322db7f35af88774
Everton Rodrigues Silva <evertosilves@gmail.com>
2023-12-30 15:51:16 -0300

    Leves mudanças

--- projeto-jogo-c/main.c
+++ projeto-jogo-c/main.c
@@ -1,11 +1,14 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 #define G 400
 #define PLAYER_JUMP_SPD 350.0f
-#define PLAYER_HOR_SPD 200.0f
+#define PLAYER_HOR_SPD 210.0f
+#define NUM_RUN_FRAMES 8
+#define NUM_JUMP_FRAMES 12
 
 typedef struct Player
 {
@@ -13,6 +16,7 @@ typedef struct Player
     float speed;
     bool canJump;
     int score;
+    int currentFrame;
 } Player;
 
 typedef struct EnvItem
@@ -22,45 +26,58 @@ typedef struct EnvItem
     Color color;
 } EnvItem;
 
-//----------------------------------------------------------------------------------
-// Module functions declaration
-//----------------------------------------------------------------------------------
 void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta);
-void UpdatePython(EnvItem *python, Rectangle *playerRect, Player *player);
+void UpdatePython(EnvItem *python, Rectangle *playerRect, Player *player, int *x, int *y, Sound point);
 void UpdateCameraCenterInsideMap(Camera2D *camera, Player *player, EnvItem *envItems, int envItemsLength, float delta, int width, int height);
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
     srand(time(NULL));
-    float elapsedTime = 30.0f;
+    float elapsedTime = 10.0f;
 
-    Texture2D playerRun = LoadTexture("/sprites/run/run1.png");
-    
     InitWindow(screenWidth, screenHeight, "Joguinho Legal");
+
+    InitAudioDevice();
+
+    Sound point = LoadSound("sounds/point.mp3");
+    Sound start = LoadSound("sounds/game-start.mp3");
+    Sound music = LoadSound("sounds/music");
+
+    PlaySound(music);
+
+    Texture2D playerRunRight[NUM_RUN_FRAMES];
+    Texture2D playerRunLeft[NUM_RUN_FRAMES];
+
+    Texture2D pythonTex = LoadTexture("./sprites/python.png");
+
+    for (int i = 0; i < NUM_RUN_FRAMES; i++)
+    {
+        char fileNameRight[64];
+        char fileNameLeft[64];
+        sprintf(fileNameRight, "./sprites/run/RunRight%i.png", i + 1);
+        sprintf(fileNameLeft, "./sprites/run/RunLeft%i.png", i + 1);
+        playerRunRight[i] = LoadTexture(fileNameRight);
+        playerRunLeft[i] = LoadTexture(fileNameLeft);
+    }
 
     Player player = {0};
     player.position = (Vector2){400, 280};
     player.speed = 0;
     player.canJump = false;
+    player.currentFrame = 0;
     EnvItem envItems[] = {
         {{0, 0, 1000, 400}, 0, LIGHTGRAY},
         {{0, 400, 1000, 200}, 1, GRAY},
         {{300, 200, 400, 10}, 1, GRAY},
         {{250, 300, 100, 10}, 1, GRAY},
         {{650, 300, 100, 10}, 1, GRAY}};
-        
 
-    int pythonPositionX = rand() % (1000 - 100 + 1) + 100;
+    int pythonPositionX = rand() % (900 - 100 + 1) + 100;
     int pythonPositionY = rand() % (300 - 100 + 1) + 100;
 
-    EnvItem python = {{pythonPositionX, pythonPositionY, 50, 40}, 0, RED};
+    EnvItem python = {{pythonPositionX, pythonPositionY, 40, 40}, 0, RED};
 
     int envItemsLength = sizeof(envItems) / sizeof(envItems[0]);
 
@@ -70,34 +87,11 @@ int main(void)
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    // Store pointers to the multiple update camera functions
-    // void (*cameraUpdaters[])(Camera2D*, Player*, EnvItem*, int, float, int, int) = {
-    //     UpdateCameraCenter,
-    //     UpdateCameraCenterInsideMap,
-    //     UpdateCameraCenterSmoothFollow,
-    //     UpdateCameraEvenOutOnLanding,
-    //     UpdateCameraPlayerBoundsPush
-    // };
-
-    // int cameraOption = 0;
-    // int cameraUpdatersLength = sizeof(cameraUpdaters)/sizeof(cameraUpdaters[0]);
-
-    // char *cameraDescriptions[] = {
-    //     "Follow player center",
-    //     "Follow player center, but clamp to map edges",
-    //     "Follow player center; smoothed",
-    //     "Follow player center horizontally; update player center vertically after landing",
-    //     "Player push camera on getting too close to screen edge"
-    // };
-
     SetTargetFPS(60);
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
     while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
+
         if (elapsedTime > 0)
         {
             float deltaTime = GetFrameTime();
@@ -106,23 +100,12 @@ int main(void)
 
             Rectangle playerRect = {player.position.x - 20, player.position.y - 40, 40, 40};
 
-            // ImageCrop(&playerRun, playerRect);
+            Vector2 playerRunPosition = {player.position.x - 50, player.position.y - 75};
+            Vector2 pythonPosition = {pythonPositionX - 10, pythonPositionY - 10};
 
-            UpdatePython(&python, &playerRect, &player);
-
-            // camera.zoom += ((float)GetMouseWheelMove()*0.05f);
-
-            // if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-            // else if (camera.zoom < 0.25f) camera.zoom = 0.25f;
-
-            // if (IsKeyPressed(KEY_C)) cameraOption = (cameraOption + 1)%cameraUpdatersLength;
-
-            // Call update camera function by its pointer
+            UpdatePython(&python, &playerRect, &player, &pythonPositionX, &pythonPositionY, point);
             UpdateCameraCenterInsideMap(&camera, &player, envItems, envItemsLength, deltaTime, screenWidth, screenHeight);
-            //----------------------------------------------------------------------------------
             elapsedTime -= deltaTime;
-            // Draw
-            //----------------------------------------------------------------------------------
             BeginDrawing();
 
             ClearBackground(LIGHTGRAY);
@@ -132,11 +115,20 @@ int main(void)
             for (int i = 0; i < envItemsLength; i++)
                 DrawRectangleRec(envItems[i].rect, envItems[i].color);
 
-            
-            DrawTexture(playerRun, player.position.y, player.position.y, RED);
+            if (IsKeyDown(KEY_RIGHT))
+            {
+                DrawTextureV(playerRunRight[player.currentFrame], playerRunPosition, WHITE);
+            }
+            else if (IsKeyDown(KEY_LEFT))
+            {
+                DrawTextureV(playerRunLeft[player.currentFrame], playerRunPosition, WHITE);
+            }
+            else
+            {
+                DrawTextureV(playerRunRight[0], playerRunPosition, WHITE);
+            }
 
-            DrawRectangleRec(playerRect, GREEN);
-            DrawRectangleRec(python.rect, python.color);
+            DrawTextureV(pythonTex, pythonPosition, WHITE);
 
             EndMode2D();
 
@@ -144,12 +136,12 @@ int main(void)
             DrawText("- Right/Left to move", 40, 40, 10, DARKGRAY);
             DrawText("- Space to jump", 40, 60, 10, DARKGRAY);
             DrawText("- Esc to exit", 40, 80, 10, DARKGRAY);
-            DrawText(TextFormat("Score: %i", (int)player.score), 600, 60, 20, BLACK);
-            DrawText(TextFormat("Tempo: %.2fs", elapsedTime), 500, 40, 20, DARKGRAY);
-
+            DrawText(TextFormat("Score: %i", (int)player.score), 600, 60, 20, DARKGRAY);
+            DrawText(TextFormat("Time: %.2fs", elapsedTime), 600, 40, 20, DARKGRAY);
 
             EndDrawing();
-        } else 
+        }
+        else
         {
             BeginDrawing();
 
@@ -158,25 +150,27 @@ int main(void)
             DrawText(TextFormat("Score: %i", (int)player.score), 250, 200, 20, BLACK);
             DrawText("Press R to Restart", 250, 225, 30, BLACK);
 
-            if (IsKeyPressed(KEY_R)) 
+            if (IsKeyPressed(KEY_R))
             {
+                PlaySound(start);
                 elapsedTime = 30.0f;
                 player.score = 0;
                 camera.zoom = 1.0f;
                 player.position = (Vector2){400, 280};
             }
-            UnloadTexture(playerRun);
             EndDrawing();
         }
-
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    // UnloadTexture(playerRun);
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    for (int i = 0; i < NUM_RUN_FRAMES; i++)
+    {
+        UnloadTexture(playerRunRight[i]);
+        UnloadTexture(playerRunLeft[i]);
+    }
+    UnloadSound(point);
+    UnloadSound(start);
+    CloseAudioDevice();
+    CloseWindow();
 
     return 0;
 }
@@ -184,9 +178,30 @@ int main(void)
 void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta)
 {
     if (IsKeyDown(KEY_LEFT))
+    {
         player->position.x -= PLAYER_HOR_SPD * delta;
+        if (player->currentFrame < 7)
+        {
+            player->currentFrame++;
+        }
+        else
+        {
+            player->currentFrame = 0;
+        }
+    }
     if (IsKeyDown(KEY_RIGHT))
+    {
         player->position.x += PLAYER_HOR_SPD * delta;
+
+        if (player->currentFrame < 7)
+        {
+            player->currentFrame++;
+        }
+        else
+        {
+            player->currentFrame = 0;
+        }
+    }
     if (IsKeyDown(KEY_SPACE) && player->canJump)
     {
         player->speed = -PLAYER_JUMP_SPD;
@@ -220,16 +235,18 @@ void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float d
         player->canJump = true;
 }
 
-void UpdatePython(EnvItem *python, Rectangle *playerRect, Player *player)
+void UpdatePython(EnvItem *python, Rectangle *playerRect, Player *player, int *x, int *y, Sound point)
 {
     bool collision = CheckCollisionRecs(*playerRect, python->rect);
 
     if (collision)
     {
-        int pythonPositionX = rand() % (900 - 100 + 1) + 100;
-        int pythonPositionY = rand() % (300 - 100 + 1) + 100;
+        *x = rand() % (900 - 100 + 1) + 100;
+        *y = rand() % (300 - 100 + 1) + 100;
 
-        python->rect = (Rectangle){pythonPositionX, pythonPositionY, 50, 40};
+        PlaySound(point);
+
+        python->rect = (Rectangle){*x, *y, 50, 40};
         player->score += 1;
     }
 }
